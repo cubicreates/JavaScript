4ac11c8f8e6f678147bca273fca80d49429e8658
Adit Ghosh <blubrdpirts@gmail.com>
2025-01-25 19:35:29 +0530

    Tutorial 17
    Array methods

---
+++ Tutorials/Chapter 5/Tutorial 17.js
@@ -0,0 +1,21 @@
+let a = [1,2,3,4,5];
+let b = a.toString();
+console.log(b);
+console.log(typeof b);
+
+let c = a.join(" ");
+console.log(c);
+
+let pp = a.pop();
+console.log(pp);
+console.log(a);
+
+let psh = a.push(1);
+console.log(a);
+
+let shft = a.shift();
+console.log(shft);
+console.log(a);
+
+let unshft = a.unshift(5);
+console.log(a);
