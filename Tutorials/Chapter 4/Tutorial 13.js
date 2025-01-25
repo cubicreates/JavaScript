fe70d8ab487b366515a6cd651b37fad1946b58e9
Adit Ghosh <blubrdpirts@gmail.com>
2025-01-25 18:40:08 +0530

    Tutorial 13
    Strings

---
+++ Tutorials/Chapter 4/Tutorial 13.js
@@ -0,0 +1,14 @@
+let greet = "Hello World";
+console.log(greet);
+
+console.log(greet.length);
+console.log(greet[0]);
+console.log(greet[1]);
+
+let boy1 = "Anvil";
+let boy2 = "Denis";
+let friend = `${boy1} and ${boy2} are friends`;
+console.log(friend);
+
+let fruit = 'Marco D\' Angelo';
+console.log(fruit);
