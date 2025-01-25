561f5769b61b8f2cb0b6555ed6d38aa8a9cffcc0
Adit Ghosh <blubrdpirts@gmail.com>
2025-01-25 13:36:37 +0530

    Tutorial 4
    Talking about Primitive Datatypes (Built-in DataTypes) and Non-Primitive DataTypes (Object).

---
+++ Tutorials/Tutorial 4.js
@@ -0,0 +1,23 @@
+// Primitive DataTypes --> Built-in Datatypes
+// NN BB SS U
+
+let a = null;
+let b = 511205;
+let c = false;
+let d = BigInt("45");
+let e = '@';
+let f = "Hello World!";
+let g = undefined
+
+console.log(a,b,c,d,e,f,g);
+console.log(typeof g);
+
+
+// Non-Primitive Datatype --> Objects
+let obj = {
+    new : "star",
+    hip : "hop"
+}
+
+console.log(obj);
+console.log(obj["new"]);
