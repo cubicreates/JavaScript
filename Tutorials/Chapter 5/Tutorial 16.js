3f4a41ccc7288c189cd98df7f0b12a1e42010f22
Adit Ghosh <blubrdpirts@gmail.com>
2025-01-25 19:27:16 +0530

    Tutorial 16
    Arrays

---
+++ Tutorials/Chapter 5/Tutorial 16.js
@@ -0,0 +1,19 @@
+let a =[1,2,3,4];
+console.log(a);
+
+let i = [null, 12, 's', "str", true, undefined]
+console.log(i);
+
+console.log(i[0]);
+console.log(i[1]);
+console.log(i[2]);
+console.log(i[3]);
+console.log(i[4]);
+console.log(i[5]);
+
+console.log(i.length);
+
+i[4] = false
+console.log(i);
+
+console.log(typeof i);
