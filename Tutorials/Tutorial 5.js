1d1d9385d7f75812c261710a79f31e6e5786f488
Adit Ghosh <blubrdpirts@gmail.com>
2025-01-25 13:58:23 +0530

    Tutorial 5
    Practice Set of 5 questions.

---
+++ Tutorials/Tutorial 5.js
@@ -0,0 +1,31 @@
+// Q1.
+let variable = "Hello ";
+let newvar = variable + 45
+console.log(newvar);
+
+// Q2.
+console.log(typeof newvar);
+
+// Q3.
+const style ={
+    Hello : "People",
+}
+console.log(style);
+
+style["Hello"] = 566;
+console.log(style);
+
+
+// Q4.
+style["Num"] = 567;
+console.log(style);
+
+// Q5.
+const dictionary = {
+    Section : "80",
+    GKMC : "Good Kid, Maad City",
+    TPAB : "To Pimp a Butterfly",
+    "DAMN." : "Politzer Prize Winner",
+    MMBS: "Mr. Morale and the Big Steppers"
+}
+console.log(dictionary);
