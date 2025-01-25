52d0850e00a89fb5f956d886576ee0f53f1c7b74
Adit Ghosh <blubrdpirts@gmail.com>
2025-01-25 14:45:41 +0530

    Tutorial 6
    Operators in JavaScript (Arithmetic, Increment, Assignment, Comparison, Logical) and Comments in JavaScript (Single Line and Multi-Line Comments)

---
+++ Tutorials/Chapter 2/Tutorial 6.js
@@ -0,0 +1,54 @@
+// Operators in JS
+
+// Arithmatic Operators
+let a = 54;
+let b = 45;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+// Increment operators
+
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(--a);
+
+// Assignment Operator
+console.log(a+=9);
+console.log(a-=9);
+console.log(a*=9);
+console.log(a/=9);
+console.log(a%=9);
+console.log(a**=2);
+
+// Comparison Operator
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>=b);
+console.log(a!=b);
+console.log(a==b);
+console.log(a===b);
+console.log(a!==b);
+console.log((a>b)?5:8);
+
+// Logical Operators
+console.log((a!=b)&&(a<b));
+console.log((a==b)||(a<b));
+console.log(!(a==b));
+
+// Coments
+
+// This is a single line comment
+/*
+This
+is
+a
+Multi-line
+comment
+*/
