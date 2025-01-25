7445da364d81dddb65237d8ef97dc321d68aeef8
Adit Ghosh <blubrdpirts@gmail.com>
2025-01-25 18:54:12 +0530

    Tutorial 15
    Practice Set

---
+++ Tutorials/Chapter 4/Tutorial 15.js
@@ -0,0 +1,20 @@
+// Q1.
+console.log("har\*".length);
+
+// Q2.
+let newstr = "String"
+console.log(newstr.includes("n"));
+console.log(newstr.startsWith("i"));
+console.log(newstr.endsWith("g"));
+
+// Q3.
+let str = "ERTTRE"
+console.log(str.toLowerCase());
+
+// Q4.
+let Statement = "Please give me rs. 1000"
+console.log(Statement.split(" ")[4]);
+
+// Q5.
+Statement[4] = "23";
+console.log(Statement);// Does not change since string is immutable.
