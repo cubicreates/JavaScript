4799c8b172080c2e8a9822fb0863486032e75e57
Adit Ghosh <blubrdpirts@gmail.com>
2025-01-25 21:35:38 +0530

    Tutorial 22
    Exercise 1

---
+++ Tutorials/Chapter 6/Tutorial 22.js
@@ -0,0 +1,20 @@
+let num = ((Math.random()*10)+1);
+
+let i=1
+while(i<=100){
+    let guess = prompt("Enter a number between 1 and 10: ");
+    
+    if (guess === num){
+        console.log("You guessed the correct number!");
+        break;
+    }
+    
+    else{
+        console.log("Try Again!");
+    };
+
+    i++;
+}
+
+console.log("Your score ",100-i);
+
