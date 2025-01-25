3be6b9bb9af11bee92822d788fbaa38be10b266e
Adit Ghosh <blubrdpirts@gmail.com>
2025-01-25 18:42:40 +0530

    Tutorial 14
    String Methods

---
+++ Tutorials/Chapter 4/Tutorial 14.js
@@ -0,0 +1,7 @@
+let banana = "banana";
+console.log(banana);
+console.log(banana.toUpperCase());
+console.log(banana.toLowerCase());
+console.log(banana.slice(1,4));
+console.log(banana.slice(1));
+console.log(banana.replace("banana","Banana"));
