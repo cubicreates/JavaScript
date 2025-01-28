ba40608771d3884adc0f6315fc1d9d591a2a4aa3
Adit Ghosh <blubrdpirts@gmail.com>
2025-01-28 20:16:05 +0530

    Tutorial 31

---
+++ Tutorials/Chapter 7/Tutorial 31/Tutorial 31.js
@@ -0,0 +1,7 @@
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.title);
+
+console.log(typeof document.documentElement);
+console.log(typeof document.head);
+console.log(typeof document.title);
