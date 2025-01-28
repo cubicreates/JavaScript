5e98bcdc6ae1e60d52b5fa1c5a5f9b59e4b48604
Adit Ghosh <blubrdpirts@gmail.com>
2025-01-28 23:04:24 +0530

    Tutorial 34

---
+++ Tutorials/Chapter 7/Tutorial 34/Tutorial 34.js
@@ -0,0 +1,9 @@
+let b = document.body;
+console.log("B first child ",b.firstChild);
+console.log("B first element child ",b.firstElementChild);
+console.log(b.previousElementSibling);
+console.log(b.nextElementSibling);
+console.log(b.firstElementChild);
+console.log(b.lastElementChild);
+
+// console.log(Array.from($0.children));
